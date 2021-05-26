f16ac178fd070ff818fa29433072d653cbf9cf69
Anonymous-hacker143 <Anonymous_hacker143@protonmail.com>
2021-05-26 19:53:30 +0530

    adding addition.c in github.c

---
+++ addition.c
@@ -0,0 +1,12 @@
+//This addition program in c
+#include<stdio.h>
+int main()
+{
+ int a,b,sum;
+  printf("Enter the value of a : \n");
+     scanf("%d"",&a);
+  printf("Enter the value of b: \n");
+    scanf("%d",&b);
+   sum =a+b;
+  printf(" Sum of the numbers is :%d",sum);
+}
